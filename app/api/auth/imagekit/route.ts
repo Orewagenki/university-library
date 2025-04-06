05bf0052ec114ab8b5bf71a44cb70a1ce69496bf
Orewagenki <orewagenki3@gmail.com>
2025-04-06 17:20:28 -0600

    Setting up imagekit

---
+++ app/api/auth/imagekit/route.ts
@@ -0,0 +1,15 @@
+import imageKit from "imagekit";
+import config from "@/lib/config";
+import { NextResponse } from "next/server";
+
+const {
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
+
+const imagekit = new imageKit({ publicKey, privateKey, urlEndpoint });
+
+export async function GET() {
+  return NextResponse.json(imagekit.getAuthenticationParameters());
+}
