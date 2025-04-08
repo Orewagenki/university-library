e235cd6d151f97f56793aab5b7ecec682262deed
Orewagenki <orewagenki3@gmail.com>
2025-04-08 08:32:42 -0600

    Update page.tsx
    Hooking up to the database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
-  <>
+const Home = async () => {
+  const result = await db.select().from(users);
+  
+  console.log(JSON.stringify(result, null, 2));
+  
+  return (
+    <>
     <BookOverview {...sampleBooks[0]} />
 
     <BookList
@@ -12,6 +19,7 @@ const Home = () => (
       containerClassName="mt-28"
     />
   </>
-);
+  )
+};
 
 export default Home;
