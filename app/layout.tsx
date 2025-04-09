cf3698aafe8e642e36f105008f7824083aecfb28
Orewagenki <orewagenki3@gmail.com>
2025-04-09 09:21:59 -0600

    Update layout.tsx
    updated layout page to take in session and wrap the body in session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 
 const ibmPlexSans = localFont({
   src: [
@@ -27,15 +28,19 @@ export const metadata: Metadata = {
     "BookWise is a book borrowing university library management solution.",
 };
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+const session = await auth();
+  
   return (
     <html lang="en">
+      <SessionProvider session={session}>
       <body
         className={`${ibmPlexSans.className} ${bebasNeue.className} antialiased`}
       >
         {children}
         <Toaster />
       </body>
+      </SessionProvider>
     </html>
   );
 };
