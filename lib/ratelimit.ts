58fd5afa09833c916b5357dbefa3982becb914f3
Orewagenki <orewagenki3@gmail.com>
2025-04-15 06:44:21 -0600

    Update ratelimit.ts
    fixed typo

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -3,7 +3,7 @@ import redis from "@/database/redis";
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.foxedWindo(5, "1m"),
+  limiter: Ratelimit.fixedWindo(5, "1m"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
