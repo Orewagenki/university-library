76ef031bd61f59138aaeb54a6c334fc11c585bac
Orewagenki <orewagenki3@gmail.com>
2025-04-16 11:54:29 -0600

    Update layout.tsx
    import styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {auth} from "@/auth";
+import { redirect} from "next/navigation";
+import '@/styles/admin.css';
 
 const Layout = async({children}: {children: ReactNode}) => {
 const session = await auth();
