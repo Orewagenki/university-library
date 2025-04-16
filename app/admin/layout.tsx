c41bbc76e7ad699fe44834f1d490c601d3fad27f
Orewagenki <orewagenki3@gmail.com>
2025-04-16 11:50:07 -0600

    Create layout.tsx
    layout page for the admin dashboard

---
+++ app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {auth} from "@/auth";
+
+const Layout = async({children}: {children: ReactNode}) => {
+const session = await auth();
+
+if(!sesson?.user?.id) redirect('/sign-in');
+  
+return(
+  <main className="flex min-h-screen w-full flex-row">
+  <p>Sidebar</p>
+    <div className="admin-container">
+    <p>Header</p>
+      {children}
+    </div>
+  </main>
+  );
+};
