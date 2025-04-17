d701a2c3c519a8423b977d2b7719f88b32fbf866
Orewagenki <orewagenki3@gmail.com>
2025-04-17 08:36:22 -0600

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 const Sidebar = () => {
-return <div>Sidebar</div>
+return (
+  <div className="admin-sidebar">
+    <div>
+      <div className="logo">
+      <Image src="/icons/admin/logo.svg" alt="logo" height={37} width={37} />
+        <h1>BookWise</h1>
+      </div>
+      <div className="mt-10 flex flex-col gap-5">
+      
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Sidebar;
