503bd5d24d9e9c0d6fba277c36d0e4a096016188
Orewagenki <orewagenki3@gmail.com>
2025-04-24 08:50:32 -0600

    Update ImageUpload.tsx
    made the upload component work for videos as well

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -31,20 +31,37 @@ const authenticator = async () => {
   }
 };
 
-const ImageUpload = ({
-  onFileChange,
-}: {
+interface Props {
+  type: 'image' | 'video';
+  accpet: string;
+  placeholder: string;
+  folder: string;
+  variant: 'dark' | 'light';
   onFileChange: (filePath: string) => void;
-}) => {
+}
+
+const ImageUpload = ({
+  type,accept,placeholder,folder,variant,
+  onFileChange,
+}: {) => {
   const ikUploadRef = useRef(null);
   const [file, setFile] = useState<{ filePath: string } | null>(null);
+  const [progress, setProgress] = useState(0);
+
+  const styles = {
+    button: === 'dark'
+    ? 'bg-dark-300'
+    : 'bg-light-600 border-gray-100 border',
+    placeholder: variant === 'dark' ? 'text-light-100' : 'text-slate-500',
+    text: variant === "dark" "text-light-100" : "text-dark-400",
+  };
 
   const onError = (error: any) => {
     console.log(error);
 
     toast({
-      title: "Image upload failed",
-      description: `Your image did not upload successfully. Please try again.`,
+      title: `${type} upload failed`,
+      description: `Your ${type} did not upload successfully. Please try again.`,
       variant: "destructive",
     });
   };
@@ -54,10 +71,35 @@ const ImageUpload = ({
     onFileChange(res.filePath);
 
     toast({
-      title: "Image uploaded successfully",
+      title: `${type} uploaded successfully`,
       description: `${res.filePath} uploaded successfully`,
     });
   };
+
+const onValidate = (file: File) => {
+if(type === 'image'){
+if(file.size > 20 * 1024 * 1024){
+toast({
+  title: "file size too large",
+  description: "Please unpload a file that is less than 20MB in size",
+  variant: "destructive",
+});
+
+  return false;
+}
+} else if (type === 'video'){
+if(file.size > 20 * 1024 * 1024){
+toast({
+  title: "file size too large",
+  description: "Please unpload a file that is less than 20MB in size",
+  variant: "destructive",
+});
+
+  return false;
+}
+}
+  return true;
+};
 
   return (
     <ImageKitProvider
@@ -70,11 +112,21 @@ const ImageUpload = ({
         ref={ikUploadRef}
         onError={onError}
         onSuccess={onSuccess}
-        fileName="test-upload.png"
+        useUniqueFileName={true}
+        validateFile={onValidate}
+        onUploadStart={() => setProgress(0)}
+        onUploadProgress{({ loaded, total }) => {
+          const percent = Math.round((loaded / total) * 100);
+
+      setProgress(percent);
+      }}
+      folder={folder}
+      accept={accept}
+        onSuccess={onSuccess}
       />
 
       <button
-        className="upload-btn"
+        className={cn("upload-btn", styles.button)}
         onClick={(e) => {
           e.preventDefault();
 
@@ -91,18 +143,34 @@ const ImageUpload = ({
           height={20}
           className="object-contain"
         />
-        <p className="text-base text-light-100">Upload a File</p>
+        <p className={cn('text-base', styles.placeholder)}>{placeholder}</p>
+        {file && <p className={cn("upload-filename",styles.text)}>{file.filePath}</p>}
         {file && <p className="upload-filename">{file.filePath}</p>}
       </button>
 
+      {progress > 0 && (
+      <div className="w-full rounded-full bg-green-200">
+        <div className="progress" style={{width: `${progress}%`}}>
+          {progress}%
+        </div>
+      </div>
+      )}
+
       {file && (
+      (type === 'image' ? (
         <IKImage
           alt={file.filePath}
           path={file.filePath}
           width={500}
           height={500}
         />
-      )}
+      ): type === 'video' ? (
+        <IKVideo
+          path={file.filePath}
+          controls={true}
+          className="h-96 w-full rounded-xl"
+          />
+      ) : null)}
     </ImageKitProvider>
   );
 };
