d97f4a5ad6f2b6954a330cee48c4c7c346ed217a
Orewagenki <orewagenki3@gmail.com>
2025-04-28 07:31:07 -0600

    Create book.ts

---
+++ lib/admin/actions/book.ts
@@ -0,0 +1,13 @@
+'use server';
+
+const createBook = async (BookParams){
+try{
+  
+}catch(error){
+  console.log(error);
+  return {
+    success: false,
+    message: "An error occurred while creating the book"
+}
+}
+}
