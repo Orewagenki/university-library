e01b35b2e25ba72d6a4220a383dbcfcd272f8feb
Orewagenki <orewagenki3@gmail.com>
2025-04-29 07:01:21 -0600

    Update page.tsx
    replacing samplebooks with books from the database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,24 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
-import { sampleBooks } from "@/constants";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 
 const Home = async () => {
-  const result = await db.select().from(users);
+  const session = await auth();
   
-  console.log(JSON.stringify(result, null, 2));
+  const latestBooks = await db
+    .select()
+    .from(books)
+    .limit(10)
+    .orderBy(desc(books.createdAt)) as Book[];
   
   return (
     <>
-    <BookOverview {...sampleBooks[0]} />
+    <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
 
     <BookList
       title="Latest Books"
-      books={sampleBooks}
+      books={latestBooks.slice(1)}
       containerClassName="mt-28"
     />
   </>
