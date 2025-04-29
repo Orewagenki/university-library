eed0287cfb81600d6c527b79b0bdb00910a95838
Orewagenki <orewagenki3@gmail.com>
2025-04-29 06:24:25 -0600

    Update book.ts
    created call to db to insert book

--- lib/admin/actions/book.ts
+++ lib/admin/actions/book.ts
@@ -1,13 +1,25 @@
 'use server';
+import { db} from "@/database/drizzle";
 
 const createBook = async (BookParams){
 try{
-  
+  const newBook = await db
+  .insert(books)
+  .values({
+    ...params,
+    availableCopies: params.totalCopies,
+  })
+  .returning();
+
+  return{
+    success: true,
+    data: JSON.parse(JSON.stringify([0])),
+  }
 }catch(error){
   console.log(error);
   return {
     success: false,
     message: "An error occurred while creating the book"
+};
 }
-}
-}
+};
