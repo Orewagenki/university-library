745d66ed0f4856f16f1679818569607adb08a762
Orewagenki <orewagenki3@gmail.com>
2025-04-29 06:50:48 -0600

    Update layout.tsx
    check if the ADMIN role is of current user and if not route them back to home page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,6 +9,15 @@ const Layout = async({children}: {children: ReactNode}) => {
 const session = await auth();
 
 if(!sesson?.user?.id) redirect('/sign-in');
+
+const isAdmin = await db
+  .select({ isAdmin: users.role })
+  .from(users)
+  .where(eq(users.id, session.user.id))
+  .limit(1)
+  .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
   
 return(
   <main className="flex min-h-screen w-full flex-row">
