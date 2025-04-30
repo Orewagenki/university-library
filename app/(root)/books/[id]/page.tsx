3313ee5229300536989c532b17a6c5c52dd59cb9
Orewagenki <orewagenki3@gmail.com>
2025-04-30 07:01:43 -0600

    Update page.tsx
    added the bookvideo component to display book trailer

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {redirect} from "next/navigation";
 import {auth} from "@/auth";
 import BookOverview from "@/components/BookOverview"
+import BookVideo from "@/components/BookVideo"
 
 const Page = ({params}: {params: Promise<{id:string}>})=>{
 const id = (await params).id;
@@ -24,7 +25,7 @@ const id = (await params).id;
   <div className="flex-[1.5]">
   <section className="flex flex-col gap-7">
   <h3>Video</h3>
-    Video Component
+    <BookVideo videoUrl={bookDetails.videoUrl}/>
   </section>
     <section className="mt-10 flex flex-col gap-7">
       <h3>Summary</h3>
